a66ff5d9582deb6491060610f7b3340ecd5c1638
AlessandroCiaccio <146941441+AlessandroCiaccio@users.noreply.github.com>
2023-11-18 22:02:19 +0100

    Updated MainTest

--- MainTest.java
+++ MainTest.java
@@ -7,16 +7,18 @@ import java.time.format.FormatStyle;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-    private final Main testing=new Main();
+    private final Main testing = new Main();
+
     @Test
     void main() {
-        OffsetDateTime data=OffsetDateTime.parse("2012-04-01T13:00:00Z");
-        System.out.println(data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
+        OffsetDateTime data = OffsetDateTime.parse("2012-04-01T13:00:00Z");
+        String fullData = (data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
 
-        System.out.println(data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
+        String mediumData = (data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
 
-        System.out.println(data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)));
-        assertEquals("domenica 1 aprile 2012\n1 apr 2012\n01/04/12",
-                "domenica 1 aprile 2012\n1 apr 2012\n01/04/12");
+        String shortData = (data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)));
+        assert fullData.equals("domenica 1 aprile 2012") : "Test superato: la data full è corretta";
+        assert mediumData.equals("1 apr 2012") : "Test superato: la data medium è corretta";
+        assert shortData.equals("01/04/12") : "Test superato: la data short è corretta";
     }
 }
